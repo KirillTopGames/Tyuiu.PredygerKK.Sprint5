f83e2f558fbb360f6b95f2e06985428d936f0ec2
Кирилл Константинович Предыгер <kirillpredyger6@gmail.com>
2024-11-22 18:26:22 +0500

    765

--- Tyuiu.PredygerKK.Sprint5.Task4.V1.Lib/DataService.cs
+++ Tyuiu.PredygerKK.Sprint5.Task4.V1.Lib/DataService.cs
@@ -1,6 +1,4 @@
-﻿using System.Globalization;
-
-using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.PredygerKK.Sprint5.Task4.V1.Lib
 {
@@ -10,7 +8,7 @@ namespace Tyuiu.PredygerKK.Sprint5.Task4.V1.Lib
         {
             double res;
             string data = File.ReadAllText(path);
-            double x = Convert.ToDouble(data);
+            double x = double.Parse(data, System.Globalization.CultureInfo.InvariantCulture); ;
             if (Math.Cos(x) == -x)
             {
                 res = 0;
@@ -20,7 +18,7 @@ namespace Tyuiu.PredygerKK.Sprint5.Task4.V1.Lib
                 res = (double)1 / (Math.Cos(x) + x) - 4.12 * x;
             }
             string res1 = Convert.ToString(res);
-            return double.Parse(res1, CultureInfo.InvariantCulture);
+            return double.Parse(res1, System.Globalization.CultureInfo.InvariantCulture);
         }
     }
 }
