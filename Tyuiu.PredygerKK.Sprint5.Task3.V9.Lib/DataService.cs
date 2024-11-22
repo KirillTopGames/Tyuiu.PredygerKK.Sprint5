b2eab69869798c6402d171e18a222138422ad9ea
Кирилл Константинович Предыгер <kirillpredyger6@gmail.com>
2024-11-22 18:11:36 +0500

    312

--- Tyuiu.PredygerKK.Sprint5.Task3.V9.Lib/DataService.cs
+++ Tyuiu.PredygerKK.Sprint5.Task3.V9.Lib/DataService.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.PredygerKK.Sprint5.Task3.V9.Lib
 
             string file = "OutPutFileTask3.bin";
 
-            double res = (x*x*x/(x*x-1));
+            double res = ((double)x*x*x/(x*x-1));
             res = Math.Round(res, 3);
             using (BinaryWriter writer = new BinaryWriter(File.Open(Path.Combine(path, file), FileMode.OpenOrCreate), Encoding.UTF8))
             {
