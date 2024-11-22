faab4846259836881e9626308b7b774ef261a184
Кирилл Предыгер <kirillpredyger6@gmail.com>
2024-11-22 16:00:41 +0500

    Task3

--- Tyuiu.PredygerKK.Sprint5.Task3.V9/Program.cs
+++ Tyuiu.PredygerKK.Sprint5.Task3.V9/Program.cs
@@ -38,8 +38,6 @@ namespace Tyuiu.PredygerKK.Sprint5.Task3.V9
 
             string res = ds.SaveToFileTextData(x);
 
-            Console.WriteLine(x*x*x/(x*x-1));
-
             Console.WriteLine("Файл: " + res);
             Console.WriteLine("Успешно создан!");
         }
