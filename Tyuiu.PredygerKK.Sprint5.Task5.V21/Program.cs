2b363e8e945fc902374e841ad6a41f3918297f45
Кирилл Константинович Предыгер <84632676+KirillTopGames@users.noreply.github.com>
2024-11-25 21:46:37 +0500

    Task5

---
+++ Tyuiu.PredygerKK.Sprint5.Task5.V21/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.PredygerKK.Sprint5.Task5.V21.Lib;
+
+namespace Tyuiu.PredygerKK.Sprint5.Task5.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Предыгер К.К. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Предыгер Кирилл Константинович | ПКТб-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти факториал наибольшего целого числа, которое делится на 2,         *");
+            Console.WriteLine("* в файле. Полученный результат вывести на консоль. У вещественных        *");
+            Console.WriteLine("* значений округлить до трёх знаков после запятой.                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask5V21.txt";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine(res);
+        }
+    }
+}
